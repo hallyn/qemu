ce0e25975d58af95516b62da6e1eaded0f570db6
Serge Hallyn <serge.hallyn@ubuntu.com>
2013-07-21 21:41:21 -0500

    When migrating a vm using vhost-net we hit the following assertion:
    qemu-kvm: /usr/src/packages/BUILD/qemu-kvm-0.15.1/hw/vhost.c:30:
    vhost_dev_sync_region: Assertion `start / (0x1000 * (8 *
    sizeof(vhost_log_chunk_t))) < dev->log_size' failed.
    
    The cases which the end < start check is intended to catch, such as
    for vga video memory, will also likely trigger the assertion.
    Reorder the code to handle this correctly.
    
    Signed-off-by: Bruce Rogers <address@hidden>
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- hw/vhost.c
+++ hw/vhost.c
@@ -26,11 +26,11 @@ static void vhost_dev_sync_region(struct vhost_dev *dev,
     vhost_log_chunk_t *to = dev->log + end / VHOST_LOG_CHUNK + 1;
     uint64_t addr = (start / VHOST_LOG_CHUNK) * VHOST_LOG_CHUNK;
 
-    assert(end / VHOST_LOG_CHUNK < dev->log_size);
-    assert(start / VHOST_LOG_CHUNK < dev->log_size);
     if (end < start) {
         return;
     }
+    assert(end / VHOST_LOG_CHUNK < dev->log_size);
+    assert(start / VHOST_LOG_CHUNK < dev->log_size);
     for (;from < to; ++from) {
         vhost_log_chunk_t log;
         int bit;
